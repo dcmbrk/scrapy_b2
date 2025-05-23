83f695fca78672f635217d4c693592e91aad974f
Forty1 <ldtung41@gmail.com>
2025-05-23 14:43:59 +0700

    Add files via upload

---
+++ vnn/vnn/pipelines.py
@@ -0,0 +1,37 @@
+from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+from vnn.mysql_helper import MySQLHelper
+from vnn.pipelines.backblaze import BackblazeB2Pipeline
+
+class VnnPipeline:
+    def __init__(self, host, database, user, password):
+        self.mysql_helper = MySQLHelper(host, database, user, password)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            host=crawler.settings.get('MYSQL_HOST'),
+            database=crawler.settings.get('MYSQL_DATABASE'),
+            user=crawler.settings.get('MYSQL_USER'),
+            password=crawler.settings.get('MYSQL_PASSWORD')
+        )
+
+    def open_spider(self, spider):
+        self.mysql_helper.connect()
+        spider.logger.info("[VnnPipeline] Connected to MySQL")
+
+    def close_spider(self, spider):
+        self.mysql_helper.close()
+        spider.logger.info("[VnnPipeline] Closed MySQL connection")
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        url = adapter.get('url')
+
+        if not url:
+            spider.logger.warning("[VnnPipeline] Item missing URL, skipping.")
+            return item
+
+        self.mysql_helper.insert_item(adapter.asdict())
+        spider.logger.info(f"[VnnPipeline] Inserted item URL: {url}")
+        return item
